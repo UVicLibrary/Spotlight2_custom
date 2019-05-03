829f7778103107e6e3de237225a0ecfa48771792
Tiffany Chan <tiff.chan29@gmail.com>
2019-05-03 15:07:36 -0700

    When creating new manifest with the range option, you can still search (but not add) items.

--- copy_over.rb
+++ copy_over.rb
@@ -1,8 +1,17 @@
-src_lines = File.readlines('spotlight14032019_23_all_users.sql')
+require 'byebug'
 
-File.open("spotlight23-new.sql", "w") do |f|
+src_lines = File.readlines('spotlight-with-passwords.sql')
+
+File.open("update-passwords.txt", "w") do |f|
   src_lines.each do |line|
-    puts line unless line.include? "guest"
-    f.puts line unless line.include? "guest"
+    if line.match(/^\((\d+)\,\'\w+@\w+\.\w+\'\,\'(.+)\',NULL/)
+      captures = line.match(/^\((\d+)\,\'\w+@\w+\.\w+\'\,\'(.+)\',NULL/).captures
+      id = captures[1]
+      password = captures[2].split("'")[0] if captures[2]
+      update_string = "UPDATE `users` SET encrypted_password='#{password}' WHERE id=#{id};"
+      f.puts update_string
+    # puts line unless line.include? "guest"
+    # f.puts line unless line.include? "guest"
+    end
   end
 end
